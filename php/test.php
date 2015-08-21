8995a3a5f91ce6cb03dfc69c4f4fbe0ad376622d
chenlei <947742853@qq.com>
2015-08-21 20:31:28 +0800

    first commit .begin learning  php

---
+++ php/test.php
@@ -0,0 +1,17 @@
+<?php
+$t=array("1"=>1111,"2"=>22222) ;
+
+
+foreach($t as $ti){
+        echo $ti ;
+}
+
+foreach($t as $k=>$ti ){
+	echo $k ;
+
+}
+$tt = array('1111','22222','333333') ;
+var_dump(array_keys($tt)) ;
+
+
+?>
